b80a57e640f12a0613aaf0ac864e207defe812aa
lrozhnov <lrozhnov@neoflex.ru>
2025-02-26 15:56:50 +0300

    init project and all modules

---
+++ src/main/java/neoflex/graduation/neoflexgraduation/NeoflexGraduationApplication.java
@@ -0,0 +1,13 @@
+package neoflex.graduation.neoflexgraduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexGraduationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoflexGraduationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neoflex/graduation/neoflexgraduation/NeoflexGraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package neoflex.graduation.neoflexgraduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexGraduationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
